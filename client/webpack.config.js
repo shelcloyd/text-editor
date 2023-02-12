bfddb8599da6c181cc78a38b306ef3ef0e1e7a94
Shel Cloyd <shel.cloyd@gmail.com>
2023-02-12 12:00:19 -0800

    webpack - more plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,31 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin'
-      })
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        start_url: './',
+        publicPath: './',
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor with offline capabilities using IndexedDB",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+        fingerprints: false,
+        inject: true,
+      }),
     ],
 
     module: {
