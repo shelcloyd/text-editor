3470ea9d722c53e9590d42ba989d786295d90c73
Shel Cloyd <shel.cloyd@gmail.com>
2023-02-12 11:01:23 -0800

    starting over - imported starter code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,39 +13,9 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.log('PUT to the database');
-
-  const jateDb = await openDB('jate', 1);
-
-  const tx = jateDb.transaction('jate', 'readwrite');
-
-  const store = tx.objectStore('jate');
-
-  const request = store.add({ jate: content });
-
-  const result = await request;
-
-  console.log('Data saved to the database', result);
-};
+export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.log('GET from the database');
-
-  const jateDb = await openDB('jate', 1);
-
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  const store = tx.objectStore('jate');
-
-  const request = store.getAll();
-
-  const result = await request;
-
-  console.log('result.value', result);
-  
-  return result;
-};
+export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
